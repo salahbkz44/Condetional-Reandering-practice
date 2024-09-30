56c6603079b5040f26081ccf53231f86f84ef693
SALAH BKZ <117542403+salahbkz44@users.noreply.github.com>
2024-09-30 11:17:11 +0100

    adding content

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Form(props) {
+  return (
+    <form className="form">
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      {!props.register && (
+        <input type="password" placeholder="Confirm Password" />
+      )}
+
+      <button type="submit">{props.register ? "Login" : "Register"}</button>
+    </form>
+  );
+}
+
+export default Form;
